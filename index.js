959a131a8a147c5a3c70ca01d1d5745404fa6b89
qchen34 <kalvinchan0415@gmail.com>
2025-03-24 23:27:25 +0800

    updated root for index

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ app.use(bodyParser.json());  // 解析 JSON 数据
 // 路由配置
 app.use('/api', recommendRoute);  // 将 /api 路径下的请求转发到 recommend 路由
 
+// 根路径测试路由
+app.get('/', (req, res) => {
+  res.send('Hello from chenqiwei.org backend!');
+});
+
 // 启动服务器
 const PORT = 3000;  // 端口号
 app.listen(PORT, () => {
